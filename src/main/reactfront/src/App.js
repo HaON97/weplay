514442f5e668626e353b46069b82296cc0b2f3c6
HaON <cainchel357@naver.com>
2023-02-07 11:35:16 +0900

    2023.02.07 송승훈 React 연동 셀레니움을 이용한 동적크롤링

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import axios from 'axios';
-
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import HeaderComponent from './components/common/HeaderComponent';
+import FooterComponent from './components/common/FooterComponent';
+import SampleComponent from './components/Sample';
 
 function App() {
 
-  // https://timeline.onstove.com/159967896
-
   return (
-    <div className="App">
-      <div>Hello</div>
+    <div>
+      <Router>
+        <HeaderComponent/>
+          <div className='container'>
+            <Routes>
+              <Route path = "/" exact element = {<SampleComponent />}></Route>
+            </Routes>
+          </div>
+        <FooterComponent/>
+      </Router>
     </div>
   );
 }
